ca5707bcce695c09bb9ae17375f31fdc7a2708a1
Maria Camila Vargas Giraldo <80228169+Camila2301@users.noreply.github.com>
2021-10-13 22:09:48 -0500

    Add files via upload

---
+++ Punto1.py
@@ -0,0 +1,42 @@
+#La siguiente funcion se queda solo con los valores que estan repetidos.
+def frecuencia(lista):
+    f = []
+    unique_list = list(set(lista)) 
+    for i in unique_list:
+        f.append(lista.count(i))
+    return len(f)
+
+#La siguiente funcion toma un numero que sea postivo y mayor a 1 y muestra ademas sus numeros primos teniendo como resultado en su salida un vector..
+def descomponer_numero(n):   
+    Numero_primo = 2
+    Numeros = []
+    
+    while n>1:
+        if n % Numero_primo == 0:# Este condicional sirve para determinar si da como residuo cero.
+            n //= Numero_primo  
+            Numeros.append(Numero_primo)
+            
+        else:
+            Numero_primo += 1 
+            
+    return Numeros #devuelve el vector de factores primos con repeticiones   
+
+
+# calcula la cantidad de enteros libres de cuadrados hasta un n
+def libre_cuadrados(n):
+    x = 0
+    for v in range(2,n+1,1):
+        lista_Numeros = descomponer_numero(v)
+        
+        if  len(lista_Numeros) == frecuencia(lista_Numeros):
+            x=x+1
+            
+        else:
+            continue
+            
+    print('Cantidad de enteros libres:', x)
+    print('P(v libre)=', x/n)
+
+
+for n in range(2,100,1):
+    print(libre_cuadrados(n))
